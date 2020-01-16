4606a0e5e68d7263b6ddabcb8ea37f21c402c186
Othmane HABCHI <othabchi@e2r8p13.42.fr>
2020-01-16 20:50:36 +0100

    main

--- main.c
+++ main.c
@@ -6,16 +6,29 @@
 /*   By: othabchi <othabchi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/25 20:31:22 by dsy               #+#    #+#             */
-/*   Updated: 2019/12/13 19:21:57 by othabchi         ###   ########.fr       */
+/*   Updated: 2020/01/16 20:50:13 by othabchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/libftprintf.h"
 #include <stdio.h>
-#include <limits.h>
+
 int main()
 {
-	printf("%d\n", ft_printf("mine : %.d ", -5));
-	printf("%d\n", printf("real : %.d ", -5));
+	printf("[%d]\n", ft_printf("%d - %i - %u - %x - %X - %c - %s - %%", -5, -5, -5, -5, -5, '\0', NULL));
+	printf("[%d]\n", printf("%d - %i - %u - %x - %X - %c - %s - %%", -5, -5, -5, -5, -5, '\0', NULL));
+
+	// printf("[%d]\n", ft_printf("%10s", "hello"));
+	// printf("[%d]\n", printf("%10s", "hello"));
+
+	// printf("[%d]\n", ft_printf("%.10s", "hello"));
+	// printf("[%d]\n", printf("%.10s", "hello"));
+
+	// printf("[%d]\n", ft_printf("%15.10s", "hello"));
+	// printf("[%d]\n", printf("%15.10s", "hello"));
+
+	// printf("[%d]\n", ft_printf("%*.*s", 15, 10, "hello"));
+	// printf("[%d]\n", printf("%*.*s", 15, 10, "hello"));
+
 	return (0);
 }
